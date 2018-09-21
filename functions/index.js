f8aed5644e7c736ec550824eae2ce8dff189be25
omjadas <omja.das@gmail.com>
2018-09-21 16:17:01 +1000

    added requestCarer signature

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+exports.requestCarer = functions.https.onCall((data, context) => {
+    // do stuff
+});
