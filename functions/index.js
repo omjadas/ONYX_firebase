753a672284ff49593ca9c8b47dd5182e705dabb6
omjadas <omja.das@gmail.com>
2018-09-22 19:30:00 +1000

    started work on requestCarer function

--- functions/index.js
+++ functions/index.js
@@ -6,5 +6,20 @@ admin.initializeApp();
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.requestCarer = functions.https.onCall((data, context) => {
+    var db = admin.firestore();
+    var user = db.collection('users').doc(context.auth.uid).get()
+        .then(doc => {
+            if (!doc.exists) {
+                console.log('No such document!');
+            } else {
+                console.log('Document data:', doc.data());
+            }
+            return;
+        })
+        .catch(err => {
+            console.log('Error getting document', err);
+        });
+    user.currentLocation;
+
     // do stuff
 });
