2b3f7795dbba8a15b5966036bf0cc0e434cd1459
omjadas <omja.das@gmail.com>
2018-09-26 09:31:44 +1000

    implemented chat notification sending, just need to add data

--- functions/index.js
+++ functions/index.js
@@ -8,3 +8,34 @@ admin.initializeApp();
 exports.requestCarer = functions.https.onCall((data, context) => {
     // do stuff
 });
+
+exports.chatNotification = functions.firestore
+    .document('chat_rooms/{chatId}/message/{messageId}')
+    .onCreate((snap, context) => {
+        const message = snap.data();
+        var db = admin.firestore();
+        db.collection('users').doc(message.receiverUid).get()
+            .then(user => {
+                if (!user.exists) {
+                    console.log('No such document!');
+                    return null;
+                } else {
+                    console.log('Document data:', user.data());
+                    return user;
+                }
+            })
+            .catch(err => {
+                console.log('Error getting document', err);
+            })
+            .then(user => {
+                var fcm = {
+                    data: {
+                        // TODO: add data
+                    },
+                    token: user.get('firebaseToken')
+                }
+                admin.messaging().send(fcm);
+                return null;
+            })
+            .catch();
+    })
